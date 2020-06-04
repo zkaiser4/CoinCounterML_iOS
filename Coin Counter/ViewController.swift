96c992da9ec356d7781f0afb62178c48b0d94ea4
zkaiser4 <zkapps10@gmail.com>
2020-06-04 17:36:32 -0500

    Initial Commit

---
+++ Coin Counter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Coin Counter
+//
+//  Created by Zain Kaiser on 6/4/20.
+//  Copyright © 2020 ZKApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
